6e066172d0ee50e8f1ef1698b5f15bedcc97d96d
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-08-31 00:07:45 +0800

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var = 1, 2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e'
+print(immutable_var)
+#immutable_var [1] = 5000
+#print(immutable_var)   кортеж не поддерживает обращение по элементам
+mutable_list = ([56, 72, 88], 0)
+print(mutable_list)
+mutable_list [0] [0] = 45
+print(mutable_list)
+
+
+
+
+
